579f001ecec856e789a85e93836dcaf50e108cd9
gistrb <gistrb9@gmail.com>
2025-12-23 21:39:05 +0700

    fix: include mobile config for EAS build

---
+++ mobile/src/config/config.js
@@ -0,0 +1,13 @@
+﻿export const API_URL = 'http://192.168.0.104:8000/api';
+
+// App Configuration
+export const APP_CONFIG = {
+  appName: 'Attendance App',
+  version: '1.0.0',
+};
+
+// Request Headers
+export const DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+  'Bypass-Tunnel-Reminder': 'true',
+};
