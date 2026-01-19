884efa8a337c4b768c3aebd00d7486c7c48ada98
gistrb <gistrb9@gmail.com>
2026-01-19 23:53:50 +0700

    Add auto createsuperuser to build script

--- backend/build.sh
+++ backend/build.sh
@@ -13,3 +13,10 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# Create superuser automatically
+# You must set DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL and DJANGO_SUPERUSER_PASSWORD in Render Environment Variables
+if [[ -n "$DJANGO_SUPERUSER_USERNAME" ]]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --noinput || echo "Superuser already exists"
+fi
